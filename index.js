41406c17a1d4f9de6557a7df1cb3de5b020cfcad
Bill-Adora <bilstone95@gmail.com>
2016-01-27 22:59:49 +0300

    First phase complete..

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    console.log("A user connected.");
-});
-
-io.on('disconnect', function(){
-    console.log("A user disconnected.");
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+    socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
